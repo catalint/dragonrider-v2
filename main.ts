c5aa1f87be47ac43ab8dcfa576a1f30255064b47
Cătălin Tănăsescu <catalint@gmail.com>
2023-04-27 22:46:40 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,83 @@
-let maxSound = 0
+
 serial.redirectToUSB()
 OLED.init(128, 64)
-let sounds: number[] = []
-let sample_rate = 100;
-let selected_souds = 15;
-loops.everyInterval(sample_rate, function () {
+let state : {
+    gameHasStarted:boolean,
+    sounds:number[],
+    secondsLeft:number,
+    maxSoundLevel:number
+} = {
+    gameHasStarted:false,
+    secondsLeft:3,
+    sounds:[],
+    maxSoundLevel:0
+}
+let sampleRate = 100;
+let selectedSouds = 15;
 
+function initGame(){
+    state = {
+        gameHasStarted: true,
+        secondsLeft:15,
+        sounds: [],
+        maxSoundLevel: 0
+    }
+}
+
+forever(()=>{
+    if (input.buttonIsPressed(Button.A)) {
+        initGame();
+    }
+})
+loops.everyInterval(1000, function () {
+    if (!state.gameHasStarted || state.secondsLeft==0) {
+        return;
+    }
+    if (state.secondsLeft>0){
+        state.secondsLeft--;
+    }
+    writeToScreen();
+})
+loops.everyInterval(sampleRate, function () {
+    if (!state.gameHasStarted) {
+        return;
+    }
     let soundLevel = input.soundLevel()
-    sounds.push(soundLevel)
+    state.sounds.push(soundLevel)
     led.plotBarGraph(
     soundLevel,
     255
     )
 })
+function writeToScreen() {
+    OLED.clear()
+    if (!state.gameHasStarted) {
+        OLED.writeStringNewLine('Salut Helikoner, indraznesti sa ma trezesti ?');
+    }else{
+        if (state.secondsLeft > 0){
+            OLED.writeStringNewLine(state.maxSoundLevel.toString())
+            OLED.writeStringNewLine("Mai ai "+state.secondsLeft.toString() +" secunde")
+        }else{
+            if (state.maxSoundLevel < 100) {
+                OLED.writeStringNewLine("GAME OVER Helikoner")
+            }
+            else {
+                OLED.writeStringNewLine("AI TREZIT DRAGONUL, esti un Helikoner puternic 💪")
+            }
+            OLED.writeStringNewLine(state.maxSoundLevel.toString())
+        }
+    }
+}
 loops.everyInterval(100, function () {
-    // sounds.sort()
-    // sounds = sounds.slice(-selected_souds)
-    // let avgMaxSound = sounds.reduce((a, b) => a + b, 0) / sounds.length;
-
-    // if (input.buttonIsPressed(Button.A)) {
-    //     basic.showNumber(avgMaxSound)
-    // } else {
-    //     if (avgMaxSound > maxSound) {
-    //         maxSound = avgMaxSound
-    //         OLED.clear()
-    //         OLED.writeStringNewLine(soundLevel.toString())
-    //     }
-    // }
+    if(!state.gameHasStarted){
+        return ;
+    }
+    state.sounds.sort()
+    state.sounds = state.sounds.slice(-selectedSouds)
+    let avgMaxSound = state.sounds.reduce((a, b) => a + b, 0) / state.sounds.length;
+    avgMaxSound = parseInt(avgMaxSound.toString())
+    state.maxSoundLevel = avgMaxSound
+    if (avgMaxSound > state.maxSoundLevel) {
+        writeToScreen();
+    }
 })
